26fcf17ad240c1cadda2e52102007f55826ed1f2
Charith Perera <charith.p@gmail.com>
2017-07-12 02:38:34 -0700

    Fix a retarded bug after following mattans isntructions

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,5 +1,5 @@
 class FoodsController < ApplicationController
-  before_action :authenticate_user!, except: [:index, :show]
+  before_action :authenticate_user!
 
   # GET /foods
   # GET /foods.json
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def show
     @user = User.find_by(username: params[:username])
-    @foods = @user.foods
+    @foods = @user.nil? ? [] : @user.foods
   end
 end
